08e033499ebe4748182d48e488920c6c8cdc3b30
YQ <yqfyanjun@163.com>
2016-02-18 21:04:26 +0800

    初始化项目

---
+++ sinaWeibo8/sinaWeibo8/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sinaWeibo8
+//
+//  Created by apple on 16/2/18.
+//  Copyright © 2016年 Qifeng Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sinaWeibo8/sinaWeibo8/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sinaWeibo8
+//
+//  Created by apple on 16/2/18.
+//  Copyright © 2016年 Qifeng Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
